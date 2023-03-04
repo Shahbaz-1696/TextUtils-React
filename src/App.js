167cf70c2f11e7e050c3b4c9bf54654039039b63
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-03-04 15:03:49 +0530

    Added About.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
-function App() {
-  return (
-    <>
-      {/* <Navbar title="TextUtils2" about="About TextUtils" /> */}
-      {/* <Navbar /> */}
-      <Navbar title="TextUtils" />
-      <div className="container my-3">
-      <TextForm heading="Enter the text to analyze below" />
-      </div>
-      
-    </>
-  );
-}
+  function App() {
+    return (
+      <>
+        {/* <Navbar title="TextUtils2" about="About TextUtils" /> */}
+        {/* <Navbar /> */}
+        <Navbar title="TextUtils" />
+        <div className="container my-3">
+        {/* <TextForm heading="Enter the text to analyze below" /> */}
+        <About />
+        </div>
+        
+      </>
+    );
+  }
 
 export default App;
