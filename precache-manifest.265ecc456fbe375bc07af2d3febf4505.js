b9d821e810693531d9af0c3d050b4b230dc7c95d
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-03-14 00:00:32 +0530

    Updates

---
+++ precache-manifest.265ecc456fbe375bc07af2d3febf4505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c973cff831141ccee146",
+    "url": "/TextUtils-React/static/css/main.ed9a0927.chunk.css"
+  },
+  {
+    "revision": "c973cff831141ccee146",
+    "url": "/TextUtils-React/static/js/main.c973cff8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2591e466b5f270c0c477",
+    "url": "/TextUtils-React/static/js/runtime~main.2591e466.js"
+  },
+  {
+    "revision": "1f551ef3d25e41e89f4f0c1dc6ae19fd",
+    "url": "/TextUtils-React/index.html"
+  }
+];
